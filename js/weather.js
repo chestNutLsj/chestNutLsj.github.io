b49733c695236644fdc5a2bc6e42217679141f81
chestNutLsj <1821552335@qq.com>
2022-01-08 20:02:05 +0800

    Site updated: 2022-01-08 20:02:04

---
+++ js/weather.js
@@ -0,0 +1,15 @@
+
+
+WIDGET = {
+  "CONFIG": {
+    "layout": "1",
+    "width": 450,
+    "height": 150,
+    "background": "1",
+    "dataColor": "E7F2B3",
+    "borderRadius": "5",
+    "modules": "10",
+    "key": "da6524b81324470d827a8a496efcbe2d"
+  }
+}
+
